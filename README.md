ba63fee01f13e53c3ffeaf151b00a57e5441c341
rbn <reuben.bond@gmail.com>
2014-01-26 21:08:13 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+webjars-angular-file-upload
+============
+
+WebJars build for angular-file-upload (https://github.com/danialfarid/angular-file-upload)
---
+++ src/main/resources/webjars-requirejs.js
@@ -0,0 +1,12 @@
+/* global requirejs */
+
+requirejs.config({
+    shim: {
+        'angular-file-upload': [ 'webjars!angular.js' ],
+        'angular-file-upload-shim': [ 'webjars!angular.js' ],
+        'angular-file-upload.min': [ 'webjars!angular.min.js' ],
+        'angular-file-upload-shim.min': [ 'webjars!angular.min.js' ],
+        'angular-file-upload-html5-shim': [ 'webjars!angular.min.js' ],
+        'angular-file-upload-html5-shim.min': [ 'webjars!angular.min.js' ]
+    }
+});
