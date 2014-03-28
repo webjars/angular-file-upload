88a1e8bef8cba052f90f1dc567b6ae120e6949ac
James Ward <james@jamesward.org>
2014-03-28 15:28:44 -0600

    new requirejs support - fixes #2

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -1,12 +1,18 @@
-/* global requirejs */
-
 requirejs.config({
+    paths: {
+        "angular-file-upload": webjars.path("angular-file-upload", "angular-file-upload"),
+        "angular-file-upload-shim": webjars.path("angular-file-upload", "angular-file-upload-shim"),
+        "angular-file-upload.min": webjars.path("angular-file-upload", "angular-file-upload.min"),
+        "angular-file-upload-shim.min": webjars.path("angular-file-upload", "angular-file-upload-shim.min"),
+        "angular-file-upload-html5-shim": webjars.path("angular-file-upload", "angular-file-upload-html5-shim"),
+        "angular-file-upload-html5-shim.min": webjars.path("angular-file-upload", "angular-file-upload-html5-shim.min")
+    },
     shim: {
-        'angular-file-upload': [ 'webjars!angular.js' ],
-        'angular-file-upload-shim': [ 'webjars!angular.js' ],
-        'angular-file-upload.min': [ 'webjars!angular.min.js' ],
-        'angular-file-upload-shim.min': [ 'webjars!angular.min.js' ],
-        'angular-file-upload-html5-shim': [ 'webjars!angular.min.js' ],
-        'angular-file-upload-html5-shim.min': [ 'webjars!angular.min.js' ]
+        "angular-file-upload": [ "angular" ],
+        "angular-file-upload-shim": [ "angular" ],
+        "angular-file-upload.min": [ "angular" ],
+        "angular-file-upload-shim.min": [ "angular" ],
+        "angular-file-upload-html5-shim": [ "angular" ],
+        "angular-file-upload-html5-shim.min": [ "angular" ]
     }
 });
